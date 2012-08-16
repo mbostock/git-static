85b85cc7ca73b55fd68ce5c1b9071167f679a302
Mike Bostock <mbostock@gmail.com>
2012-08-16 11:24:42 -0700

    Remove duplicate code. Handle missing files.

--- index.js
+++ index.js
@@ -12,38 +12,33 @@ gitteh.openRepository(path.join(__dirname, "repository", ".git"), function(error
       if (error) return response.end(error + "");
       reference.resolve(function(error, reference2) {
         if (error) return response.end(error + "");
-        repository.getCommit(reference2.target, function(error, commit) {
-          if (error) return response.end(error + "");
-          repository.getTree(commit.tree, function(error, tree) {
-            if (error) return response.end(error + "");
-            tree.entries.forEach(function(entry) {
-              if (entry.name === request.params[0]) {
-                repository.getBlob(entry.id, function(error, blob) {
-                  response.end(blob.data.toString("UTF-8"));
-                });
-              }
-            });
-          });
-        });
+        serveFile(reference2.target, request.params[0], response);
       });
     });
   });
 
   server.get(/^\/([a-f0-9]{40})\/(.*)/, function(request, response) {
-    repository.getCommit(request.params[0], function(error, commit) {
+    serveFile(request.params[0], request.params[1], response);
+  });
+
+  function serveFile(sha1, file, response) {
+    repository.getCommit(sha1, function(error, commit) {
       if (error) return response.end(error + "");
       repository.getTree(commit.tree, function(error, tree) {
         if (error) return response.end(error + "");
+        var found = false;
         tree.entries.forEach(function(entry) {
-          if (entry.name === request.params[1]) {
+          if (entry.name === file) {
             repository.getBlob(entry.id, function(error, blob) {
               response.end(blob.data.toString("UTF-8"));
             });
+            found = true;
           }
         });
+        if (!found) response.end("File not found.");
       });
     });
-  });
+  }
 
   server.listen(3000);
 });
