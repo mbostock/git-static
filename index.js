5584eca39fe019e0fc2f1ff0c4f93d245ab32950
Mike Bostock <mbostock@gmail.com>
2012-08-16 11:21:44 -0700

    Support for HEAD.

--- index.js
+++ index.js
@@ -5,43 +5,45 @@ var express = require("express"),
 gitteh.openRepository(path.join(__dirname, "repository", ".git"), function(error, repository) {
   if (error) throw error;
 
-  repository.getIndex(function(error, index) {
-    if (error) throw error;
+  var server = express();
 
-    var server = express();
-
-    server.get(/^\/HEAD\/(.*)/, function(request, response) {
-      index.findEntry(request.params[0], function(error, entry) {
-        repository.getBlob(entry.oid, function(error, blob) {
-          response.end(blob.data.toString("UTF-8"));
-        });
-      });
-    });
-
-    server.get(/^\/([a-f0-9]{40})\/(.*)/, function(request, response) {
-      repository.getCommit(request.params[0], function(error, commit) {
+  server.get(/^\/HEAD\/(.*)/, function(request, response) {
+    repository.getReference("HEAD", function(error, reference) {
+      if (error) return response.end(error + "");
+      reference.resolve(function(error, reference2) {
         if (error) return response.end(error + "");
-        repository.getTree(commit.tree, function(error, tree) {
+        repository.getCommit(reference2.target, function(error, commit) {
           if (error) return response.end(error + "");
-          tree.entries.forEach(function(entry) {
-            if (entry.name === request.params[1]) {
-              repository.getBlob(entry.id, function(error, blob) {
-                response.end(blob.data.toString("UTF-8"));
-              });
-            }
+          repository.getTree(commit.tree, function(error, tree) {
+            if (error) return response.end(error + "");
+            tree.entries.forEach(function(entry) {
+              if (entry.name === request.params[0]) {
+                repository.getBlob(entry.id, function(error, blob) {
+                  response.end(blob.data.toString("UTF-8"));
+                });
+              }
+            });
           });
         });
       });
     });
-
-    // server.get("/", function(request, response) {
-    //     repository.getBlob("9b4b40c2bca67e781930105fa190b9b90235cfe5", function(error, blob) {
-    //       if (error) return response.end(error.toString());
-    //       response.end(blob.data.toString("UTF-8"));
-    //     });
-    //   });
-    // });
-
-    server.listen(3000);
   });
+
+  server.get(/^\/([a-f0-9]{40})\/(.*)/, function(request, response) {
+    repository.getCommit(request.params[0], function(error, commit) {
+      if (error) return response.end(error + "");
+      repository.getTree(commit.tree, function(error, tree) {
+        if (error) return response.end(error + "");
+        tree.entries.forEach(function(entry) {
+          if (entry.name === request.params[1]) {
+            repository.getBlob(entry.id, function(error, blob) {
+              response.end(blob.data.toString("UTF-8"));
+            });
+          }
+        });
+      });
+    });
+  });
+
+  server.listen(3000);
 });
