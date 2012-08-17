000c0b155885d49de743dfcf874774020c4cdf6f
Mike Bostock <mbostock@gmail.com>
2012-08-16 22:18:22 -0700

    Open repository per-request.
    Apparently, keeping the repository open prevents libgit2 from seeing updates?

--- index.js
+++ index.js
@@ -3,12 +3,11 @@ var express = require("express"),
     mime = require("mime"),
     path = require("path");
 
-gitteh.openRepository(path.join(__dirname, "repository"), function(error, repository) {
-  if (error) throw error;
+var server = express();
 
-  var server = express();
-
-  server.get(/^\/HEAD\/(.*)/, function(request, response) {
+server.get(/^\/HEAD\/(.*)/, function(request, response) {
+  gitteh.openRepository(path.join(__dirname, "repository"), function(error, repository) {
+    if (error) return serveError(500, error + "", response);
     repository.getReference("HEAD", function(error, reference) {
       if (error) return serveError(500, error + "", response);
       reference.resolve(function(error, reference) {
@@ -17,12 +16,15 @@ gitteh.openRepository(path.join(__dirname, "repository"), function(error, reposi
       });
     });
   });
+});
 
-  server.get(/^\/([a-f0-9]{40})\/(.*)/, function(request, response) {
-    serveFile(request.params[0], request.params[1], response);
-  });
+server.get(/^\/([a-f0-9]{40})\/(.*)/, function(request, response) {
+  serveFile(request.params[0], request.params[1], response);
+});
 
-  function serveFile(sha1, file, response) {
+function serveFile(sha1, file, response) {
+  gitteh.openRepository(path.join(__dirname, "repository"), function(error, repository) {
+    if (error) return serveError(500, error + "", response);
     repository.getCommit(sha1, function(error, commit) {
       if (error) return serveError(500, error + "", response);
       repository.getTree(commit.tree, function(error, tree) {
@@ -44,16 +46,16 @@ gitteh.openRepository(path.join(__dirname, "repository"), function(error, reposi
         }
       });
     });
-  }
+  });
+}
 
-  function serveError(code, message, response) {
-    response.writeHead(code, {"Content-Type": "text/plain"});
-    response.end(message);
-  }
+function serveError(code, message, response) {
+  response.writeHead(code, {"Content-Type": "text/plain"});
+  response.end(message);
+}
 
-  function text(type) {
-    return /^(text\/)|(application\/(javascript|json)|image\/svg$)/.test(type);
-  }
+function text(type) {
+  return /^(text\/)|(application\/(javascript|json)|image\/svg$)/.test(type);
+}
 
-  server.listen(3000);
-});
+server.listen(3000);
